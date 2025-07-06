f449e2d316f7fa5279cbd622b315fbb8d8afef86
Pohaval <64406947+Pohaval@users.noreply.github.com>
2025-07-06 19:48:32 +0700

    Update wsgi.py

--- flaskapp/wsgi.py
+++ flaskapp/wsgi.py
@@ -1,9 +1,3 @@
-print("Hello world")
-from flask import Flask
-app = Flask(__name__)
-#декоратор для вывода страницы по умолчанию
-@app.route("/")
-def hello():
-return " <html><head></head> <body> Hello World! </body></html>"
+from some_app import app
 if __name__ == "__main__":
-app.run(host='127.0.0.1',port=5000)
+  app.run()
